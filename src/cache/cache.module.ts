a02604dfff01467cb240268bc365e7485f9c0393
erik-martins <erik.ozzetti.martins@gmail.com>
2024-10-08 16:32:38 -0300

    fix:  provider and exports

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CacheModule } from '@nestjs/cache-manager';
 import * as redisStore from 'cache-manager-redis-store';
+import { CacheService } from './cache.service';
 @Module({
   imports: [
     CacheModule.register({
@@ -10,5 +11,7 @@ import * as redisStore from 'cache-manager-redis-store';
       port: 6379,
     }),
   ],
+  providers: [CacheService],
+  exports: [CacheService],
 })
 export class CacheModuleProject {}
