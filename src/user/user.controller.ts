33469555b6f69bc251df717a5f447fe81e47ab13
erik-martins <erik.ozzetti.martins@gmail.com>
2024-10-08 11:38:52 -0300

    feat: controller  metode post  create  user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserRequestDto } from './dto/user-request.dto';
+import { UserMapper } from './mapper/user.mapper';
 
 @Controller('api/v1/users')
-export class UserController {}
+export class UserController {
+  constructor() {}
+
+  @Post('/create')
+  async create(@Body() user: UserRequestDto) {
+    const userEntity = UserMapper.toEntity(user);
+    return userEntity;
+  }
+}
