5037843e82198f6345a4db34df9429e6284eeb09
erik-martins <erik.ozzetti.martins@gmail.com>
2024-10-08 16:33:36 -0300

    feat:  doc swagger

--- src/user/dto/user-request.dto.ts
+++ src/user/dto/user-request.dto.ts
@@ -1,14 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class UserRequestDto {
+  @ApiProperty({
+    description: 'User name',
+    example: 'John Doe',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'User email',
+    example: 'test@email.com',
+  })
   @IsString()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    description: 'User password',
+    example: 'password',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(6, {
--- src/user/dto/user-response.dto.ts
+++ src/user/dto/user-response.dto.ts
@@ -1,11 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UserResponseDto {
+  @ApiProperty({
+    type: 'number',
+    description: 'The id of the user',
+    example: 1,
+  })
   id: number;
-
+  @ApiProperty({
+    type: 'string',
+    description: 'The name of the user',
+    example: 'John Doe',
+  })
   name: string;
-
+  @ApiProperty({
+    type: 'string',
+    description: 'The email of the user',
+    example: 'teste@email.com',
+  })
   email: string;
-
+  @ApiProperty({
+    type: 'date',
+    description: 'The date of the user was created',
+    example: '2021-08-31T03:00:00.000Z',
+  })
   createAt: Date;
-
+  @ApiProperty({
+    type: 'date',
+    description: 'The date of the user was updated',
+    example: '2021-08-31T03:00:00.000Z',
+  })
   updateAt: Date;
 }
