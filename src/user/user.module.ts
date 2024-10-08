926e0fa49ba283e795868f15766e0c57418f9285
erik-martins <erik.ozzetti.martins@gmail.com>
2024-10-08 16:35:56 -0300

    feat: user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { UserRepositoryImpl } from './repository/user-repository-impl';
+import { CreateUserUseCase } from './usecases/create-user.usecase';
+import { ListUsersUseCase } from './usecases/list-users.usecase';
+import { DeleteUserUseCase } from './usecases/delete-user.usecase';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entity/user.entity';
+import { CacheModuleProject } from '../cache/cache.module';
+import { GetByIdUserUseCase } from './usecases/get-by-id-user.usecase';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity]), CacheModuleProject],
   controllers: [UserController],
+  providers: [
+    UserRepositoryImpl,
+    CreateUserUseCase,
+    ListUsersUseCase,
+    CreateUserUseCase,
+    DeleteUserUseCase,
+    GetByIdUserUseCase,
+  ],
 })
 export class UserModule {}
