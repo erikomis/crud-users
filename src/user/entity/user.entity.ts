c9492a345c2f33f758494b5121a5432f661ac1d2
erik-martins <erik.ozzetti.martins@gmail.com>
2024-10-08 16:08:47 -0300

    feat:  entity user typeorm

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,6 +1,29 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('user')
 export class UserEntity {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   name: string;
+  @Column({
+    unique: true,
+  })
   email: string;
+  @Column()
   password: string;
+  @Column({
+    name: 'create_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
+
+  @Column({
+    name: 'update_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
 }
