90692bb2c3851834fa9b8ac555b9a62931849955
erik-martins <erik.ozzetti.martins@gmail.com>
2024-10-08 16:10:02 -0300

    feat: migration

---
+++ src/migrations/1728400385349-create_tables-user-log.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTablesUserLog1728400385349 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(` 
+        CREATE TABLE public.user (
+            id integer PRIMARY KEY,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP NOT NULL DEFAULT now(),
+            updated_at TIMESTAMP NOT NULL DEFAULT now(),
+            CONSTRAINT "user_email_key" UNIQUE ("email")
+        );
+        `);
+    await queryRunner.query(
+      ` 
+            CREATE TABLE public.log (
+                id SERIAL PRIMARY KEY,
+                data TEXT NOT NULL,
+                created_at TIMESTAMP NOT NULL DEFAULT now()
+            );
+    `,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user";`);
+    await queryRunner.query(`DROP TABLE "log";`);
+  }
+}
