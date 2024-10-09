202d769917763f762d8d1036949b52c8d1ad7d39
erik-martins <erik.ozzetti.martins@gmail.com>
2024-10-08 21:51:22 -0300

    feat: kafka  mensageria

--- src/user/usecases/update-user.usecase.ts
+++ src/user/usecases/update-user.usecase.ts
@@ -2,12 +2,14 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UserRepositoryImpl } from '../repository/user-repository-impl';
 import { CacheService } from '../../cache/cache.service';
 import { UserEntity } from '../entity/user.entity';
+import { KafkaProducer } from '../../kafka/producer/kafka.producer';
 
 @Injectable()
 export class UpdateUserUseCase {
   constructor(
     private userRepository: UserRepositoryImpl,
     private cacheManager: CacheService,
+    private kafkaProducer: KafkaProducer,
   ) {}
   private readonly userListCacheKey = 'user_list';
   async execute(id: number, user: UserEntity): Promise<UserEntity> {
@@ -18,7 +20,15 @@ export class UpdateUserUseCase {
     }
     user.id = id;
 
-    const userUpdate = this.userRepository.update(user);
+    const userUpdate = await this.userRepository.update(user);
+    await this.kafkaProducer.sendMessage('my-kafka-consumer', {
+      id: userUpdate.id,
+      name: userUpdate.name,
+      email: userUpdate.email,
+      createAt: userUpdate.createAt,
+      updateAt: userUpdate.updateAt,
+      action: 'update',
+    });
 
     await this.cacheManager.deleteCache(`user:${id}`);
 
