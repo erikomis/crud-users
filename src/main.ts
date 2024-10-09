870ff6e83132b5fe81e4ddc82d162c372ffa5b46
erik-martins <erik.ozzetti.martins@gmail.com>
2024-10-08 21:53:02 -0300

    feat: MicroserviceOptions

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      },
+    },
+  });
   const config = new DocumentBuilder()
     .setTitle('Users API')
     .setDescription('The users API description')
     .setVersion('1.0')
     .addTag('users')
     .build();
-
+  await app.startAllMicroservices();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
